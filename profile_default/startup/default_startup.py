d19c4df8d4df9625371d77e6095137a75ced889b
jackjackk <jackjackk@gmail.com>
2015-11-17 12:02:55 +0000

    first commit

---
+++ profile_default/startup/default_startup.py
@@ -0,0 +1,44 @@
+# OS libraries
+import os
+import glob
+import sys
+import subprocess as sproc
+
+# Core scientific libraries
+import numpy as np
+import scipy as sp
+import pandas as pd
+
+# Machine learning libraries
+import sklearn as sk
+import sklearn.cluster as skc
+import sklearn.datasets as skd
+import sklearn.metrics as skm
+import sklearn.preprocessing as skp
+import sklearn.pipeline as skpip
+import sklearn.decomposition as skdecomp
+import sklearn.gaussian_process as skgp
+
+# Statistics libraries
+import patsy as pa
+import scipy.signal as sps
+import statsmodels.formula.api as smf
+
+# Plot libraries
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+# Utilities libraries
+from sklearn.externals import joblib
+from pprint import pprint
+import time
+import re
+import logging as lg
+import itertools as it
+import pdb
+
+# GAMS & WITCH
+import gdxpy as gp
+import witchpy as w
+import sspdb as ssp
